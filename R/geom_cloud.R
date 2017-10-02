eadcffd68d61c44de9d1d23b139dc8ffb9f683bc
Steven E. Pav <shabbychef@gmail.com>
2017-10-02 22:24:58 +0000

    version 0.1.1

---
+++ R/geom_cloud.R
@@ -0,0 +1,192 @@
+# This file is part of ggallin.
+#
+# ggallin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ggallin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ggallin.  If not, see <http://www.gnu.org/licenses/>.
+
+# Created: 2017.09.24
+# Copyright: Steven E. Pav, 2017
+# Author: Steven E. Pav <shabbychef@gmail.com>
+# Comments: Steven E. Pav
+
+# get points equally spaced in density 
+.equal_ses <- function(steps) {
+	xend <- c(0,4)
+	endpnts <- dnorm(xend)
+# perhaps use ppoints instead?
+	deql <- seq(from=endpnts[1],to=endpnts[2],length.out=steps+1)
+	davg <- (deql[-1] + deql[-length(deql)])/2
+# invert
+	xeql <- unlist(lapply(davg,function(d) {
+					 uniroot(f=function(x) { dnorm(x) - d },interval=xend)$root
+	}))
+	xeql
+}
+
+#' @title geom_cloud 
+#'
+#' @description 
+#'
+#' Draw a normal uncertainty cloud as a ribbon
+#'
+#' Draws overlapping ribbons of the same identity to create 
+#' a cloud of (Gaussian) uncertainty. Similar to an errorbar geom in
+#' use, but visually less distracting (sometimes).
+#'
+#' @section Aesthetics:
+#' \code{geom_cloud} understands the following aesthetics (required aesthetics
+#' are in bold):
+#' \itemize{
+#'   \item \strong{\code{x}}
+#'   \item \strong{\code{y}}
+#'   \item \strong{\code{ymin}}
+#'   \item \strong{\code{ymax}}
+#'   \item \code{fill}
+#' }
+#' Only one of \code{ymin} and \code{ymax} is strictly required.
+#'
+#' @details
+#'
+#' Assumes that \code{ymin} and \code{ymax} are plotted at a 
+#' fixed number of standard errors away from \code{y}, then computes
+#' a Gaussian density with that standard deviation, plotting a cloud
+#' (based on \code{geom_ribbon}) with alpha proportional to the density.
+#' This appears as a vertical \sQuote{cloud} of uncertainty. In use,
+#' this geom should be comparable to \code{geom_errorbar}.
+#'
+#' A sample output from \code{geom_cloud}:
+#'
+#' \if{html}{
+#' \figure{geom_cloud-1.png}{options: width="100\%" alt="Figure: geom_cloud-1.png"}
+#' }
+#' \if{latex}{
+#' \figure{geom_cloud-1.png}{options: width=10cm}
+#' }
+#'
+#' @inheritParams ggplot2::geom_ribbon
+#' @param steps The integer number of steps, or equivalently, the number of
+#'        overlapping ribbons. A larger number makes a smoother cloud
+#'        at the possible expense of rendering time. Values larger than
+#'        around 20 are typically not necessary.
+#' @param max_alpha The maximum alpha at the maximum density. The cloud
+#'        will have alpha no greater than this value.
+#' @param se_mult The \sQuote{multiplier} of standard errors of the given
+#'        \code{ymin} and \code{ymax}. If these are at one standard error,
+#'        then let \code{se_mult} take the default value of 1.
+#'
+#' @keywords plotting
+#'
+#' @seealso
+#'  \code{\link[ggplot2]{geom_ribbon}}: The underlying geom
+#' @note
+#' This is a thin wrapper on the \code{geom_ribbon} geom.
+#' @template etc
+#' @export
+#' @name geom_cloud
+#' @rdname geom_cloud
+#' @examples
+#' set.seed(2134)
+#' nobs <- 200
+#' mydat <- data.frame(grp=sample(c(0,1),nobs,replace=TRUE),
+#'   colfac=sample(letters[1:2],nobs,replace=TRUE),
+#'   rowfac=sample(letters[10 + (1:3)],nobs,replace=TRUE)) 
+#' mydat$x <- seq(0,1,length.out=nobs) + 0.33 * mydat$grp
+#' mydat$y <- 0.25 * rnorm(nobs) + 2 * mydat$grp
+#' mydat$grp <- factor(mydat$grp)
+#' mydat$se  <- sqrt(mydat$x)
+#'
+#' ggplot(mydat,aes(x=x,y=y,ymin=y-se,ymax=y+se,color=grp)) +
+#' facet_grid(rowfac ~ colfac) + 
+#' geom_line() + 
+#' geom_errorbar() + 
+#' labs(title='uncertainty by errorbar')
+#'
+#' ggplot(mydat,aes(x=x,y=y,ymin=y-se,ymax=y+se,fill=grp)) +
+#' facet_grid(rowfac ~ colfac) + 
+#' geom_line() + 
+#' geom_cloud(steps=15,max_alpha=0.85) +
+#' labs(title='uncertainty by cloudr')
+#' @importFrom grid gList
+#' @importFrom ggplot2 layer
+#' @importFrom stats approxfun dnorm uniroot
+library(ggplot2)
+library(grid)
+
+geom_cloud <- function(mapping = NULL, data = NULL, ...,
+											 na.rm = TRUE,
+											 steps = 7, se_mult=1, max_alpha=1,
+											 inherit.aes = TRUE) {
+
+  layer(
+    data = data,
+    mapping = mapping,
+    stat = "identity",
+    geom = GeomCloud,
+    position = "identity",
+    inherit.aes = inherit.aes,
+    params = list(
+      na.rm = na.rm,
+			steps = steps,
+			se_mult = se_mult,
+			max_alpha = max_alpha,
+      ...
+    )
+  )
+}
+
+
+#' Geom Proto
+#' @rdname geom_cloud
+#' @format NULL
+#' @usage NULL
+#' @keywords internal
+#' @export
+GeomCloud <- ggproto("GeomCloud", Geom,
+  required_aes = c("x", "y", "ymin", "ymax"),
+  non_missing_aes = c("fill"),
+  default_aes = aes(
+    colour = NA, fill = NA, alpha = 1, size=1, linetype=1
+  ),
+	setup_data = function(data,params) {
+		if (params$na.rm) {
+			ok_row <- !(is.na(data$x) | is.na(data$y) | (is.na(data$ymin) & is.na(data$ymax)))
+			data <- data[ok_row,]
+		}
+		ses <- .equal_ses(params$steps)
+		data$up_se <- (1/params$se_mult) * (data$ymax - data$y)
+		data$dn_se <- (1/params$se_mult) * (data$y - data$ymin)
+		# a trick to get positions ok: puff up the ymax and ymin for now
+		maxse <- max(ses)
+		data$ymax  <- data$y + maxse * data$up_se
+		data$ymin  <- data$y - maxse * data$dn_se
+		data
+	},
+  draw_group = function(data, panel_scales, coord,
+												na.rm = TRUE,
+												steps = 5, se_mult=1, max_alpha=1) {
+
+		data$alpha <- max_alpha / steps
+		# 2FIX: use the coordinate transform? or just forget it?
+		ses <- .equal_ses(steps)
+		grobs <- Map(function(myse) {
+									 this_data <- data
+									 this_data$ymax <- this_data$y + myse * this_data$up_se
+									 this_data$ymin <- this_data$y - myse * this_data$dn_se
+									 ggplot2::GeomRibbon$draw_group(this_data, panel_scales, coord, na.rm=na.rm)
+												},ses)
+		do.call("gList",grobs)
+  },
+	draw_key = draw_key_polygon
+)
+
+#for vim modeline: (do not edit)
+# vim:fdm=marker:fmr=FOLDUP,UNFOLD:cms=#%s:syn=r:ft=r
---
+++ R/transforms.R
@@ -0,0 +1,245 @@
+# This file is part of ggallin.
+#
+# ggallin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ggallin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ggallin.  If not, see <http://www.gnu.org/licenses/>.
+
+# Created: 2017.09.25
+# Copyright: Steven E. Pav, 2017
+# Author: Steven E. Pav <shabbychef@gmail.com>
+# Comments: Steven E. Pav
+
+#' @title Various scale transforms.
+#'
+#' @description 
+#'
+#' Various scale transformations.
+#'
+#' @details
+#'
+#' The available transforms:
+#' \itemize{
+#' \item \code{ssqrt_trans} a signed square root transform appropriate for
+#' negative or positive numbers.
+#' \item \code{pseudolog10_trans} an \code{asinh} transformation, which is like
+#' a logarithm, but appropriate for negative or positive numbers.  This
+#' transformation was taken from the Win Vector blog, 
+#' \url{http://www.win-vector.com/blog/2012/03/modeling-trick-the-signed-pseudo-logarithm/}.
+#' }
+#'
+#' @keywords plotting
+#' @seealso \code{\link[scales]{trans_new}}.
+#' @return 
+#' A scale transformation object.
+#'
+#' @examples 
+#' set.seed(1234)
+#' ggplot(data.frame(x=rnorm(100),y=runif(100)),aes(x=x,y=y)) + 
+#'   geom_point() + 
+#'   scale_x_continuous(trans=ssqrt_trans)
+#'
+#' set.seed(1234)
+#' ggplot(data.frame(x=rnorm(100),y=runif(100)),aes(x=x,y=y)) + 
+#'   geom_point() + 
+#'   scale_x_continuous(trans=pseudolog10_trans)
+#'
+#' @template etc
+#' @rdname misc_transforms
+#' @export
+ssqrt_trans <- scales::trans_new(name      = 'signed square root',
+																 transform = function(x) sign(x) * sqrt(abs(x)),
+																 inverse   = function(y) sign(y) * y^2,
+																 domain    = c(-Inf,Inf))
+
+#' @seealso \url{http://www.win-vector.com/blog/2012/03/modeling-trick-the-signed-pseudo-logarithm/}
+#' @export
+#' @rdname misc_transforms
+pseudolog10_trans <- scales::trans_new(name      = 'pseudo log10',
+																			 transform = function(x) asinh(x/2)/log(10),
+																			 inverse   = function(y) 2 * sinh(y * log(10)),
+																			 domain    = c(-Inf,Inf))
+
+
+#' @title Interpolation based scale transforms.
+#'
+#' @description 
+#'
+#' Interpolation based scale transformations. The user supplies \eqn{x} and
+#' \eqn{y} (which should be monotonic increasing or decreasing in \eqn{x}) 
+#' to create a scale transformation based on linear interpolation. 
+#'
+#' A \sQuote{warp} transformation is also supported wherein the user supplies
+#' \eqn{x} and \eqn{w} where, after sorting on \eqn{x}, the cumulative sum
+#' of \eqn{w} are used as the \eqn{y} in an interpolation transformation.
+#' Here \eqn{w} are the rate of increase, or \sQuote{weights}.
+#'
+#' @keywords plotting
+#' @seealso \code{\link[scales]{trans_new}}.
+#' @return 
+#' A scale transformation object.
+#'
+#' @examples 
+#' set.seed(1234)
+#' ggplot(data.frame(x=rnorm(100),y=runif(100)),aes(x=x,y=y)) + 
+#'   geom_point() + 
+#'   scale_x_continuous(trans=interp_trans(x=seq(-10,10,by=1),y=cumsum(runif(21))))
+#'
+#' set.seed(1234)
+#' ggplot(data.frame(x=rnorm(100),y=runif(100)),aes(x=x,y=y)) + 
+#'   geom_point() + 
+#'   scale_x_continuous(trans=warp_trans(x=seq(-10,10,by=1),w=runif(21)))
+#'
+#' # equivalently:
+#' set.seed(1234)
+#' ggplot(data.frame(x=rnorm(100),y=runif(100)),aes(x=x,y=y)) + 
+#'   geom_point() + 
+#'   scale_x_continuous(trans=warp_trans(data=data.frame(x=seq(-10,10,by=1),w=runif(21))))
+#'
+#' # this is like trans_sqrt:
+#' set.seed(1234)
+#' myx <- seq(0,5,by=0.01)
+#' ggplot(data.frame(x=rnorm(100),y=runif(100)),aes(x=x,y=y)) + 
+#'   geom_point() + 
+#'   scale_y_continuous(trans=interp_trans(x=myx,y=sqrt(myx)))
+#'
+#' @template etc
+#' @rdname interpolation_transforms
+#' @param x  the \eqn{x} coordinates for linear interpolation. 
+#' @param y  the \eqn{y} coordinates for linear interpolation. 
+#' @param data  A \code{data.frame} with columns of \code{x} and \code{y}
+#' for \code{interp_trans} or \code{x} and \code{w} for
+#' \code{warp_trans}. If \code{data} is given, it takes precedence over
+#' the given \code{x, y, w}.
+#' @param na.rm If \code{TRUE}, then missing \code{x} or \code{y} will
+#' be removed.
+#' @inheritParams scales::trans_new
+#' @usage interp_trans(x=NULL,y=NULL,data=NULL,na.rm=TRUE,breaks=NULL,format=NULL)
+#' 
+#' @export
+interp_trans <- function(x=NULL,y=NULL,data=NULL,na.rm=TRUE,breaks=NULL,format=NULL) {
+	if (!is.null(x) && is.data.frame(x) && is.null(y) && is.null(data)) {
+		# wrong order!
+		data <- x
+		x <- NULL
+	}
+	if (!is.null(data) && is.data.frame(data)) {
+		stopifnot(all(c('x','y') %in% colnames(data)))
+		x <- data$x
+		y <- data$y
+	}
+	if (na.rm) {
+		okxy <- !(is.na(x) | is.na(y))
+		x <- x[okxy]
+		y <- y[okxy]
+	}
+	afun <- approxfun(x=y,y=x,method='linear',rule=2)
+	datex <- ('Date' %in% class(x))
+
+	if (datex) {
+		tfun <- approxfun(x=as.numeric(x),y=y,method='linear',rule=2)
+		transfo <- function(x) tfun(as.numeric(x))
+	} else {
+		transfo <- approxfun(x=x,y=y,method='linear',rule=2)
+	}
+	if (datex) {
+		ifun <- function(y) { as.Date(afun(y),origin=as.POSIXct('1970-01-01',tz='UTC')) }
+	} else {
+		ifun <- afun
+	}
+
+	domain <- c(min(as.numeric(x)),max(as.numeric(x)))
+	if (is.null(breaks)) { breaks <- scales::trans_breaks(transfo,afun) }
+	if (is.null(format)) { format <- scales::format_format() }
+	scales::trans_new(name      = 'interpolated scale',
+										transform = transfo,
+										inverse   = ifun,
+										breaks    = breaks,
+										format    = format,
+										domain    = domain)
+}
+
+#' @param w  the \eqn{w} coordinates for the \sQuote{warp} interpolation.
+#' The cumulative sum of \code{w} are computed then fed to the
+#' interpolation transform.
+#' @usage warp_trans(x=NULL,w=NULL,data=NULL,na.rm=TRUE,breaks=NULL,format=NULL) 
+#' @export
+#' @rdname interpolation_transforms
+warp_trans <- function(x=NULL,w=NULL,data=NULL,na.rm=TRUE,breaks=NULL,format=NULL) {
+	if (!is.null(x) && is.data.frame(x) && is.null(w) && is.null(data)) {
+		# wrong order!
+		data <- x
+		x <- NULL
+	}
+	if (!is.null(data) && is.data.frame(data)) {
+		stopifnot(all(c('x','w') %in% colnames(data)))
+		x <- data$x
+		w <- data$w
+	}
+	if (na.rm) {
+		okxy <- !(is.na(x) | is.na(w))
+		x <- x[okxy]
+		w <- w[okxy]
+	}
+	# check monotonicity
+	stopifnot(all(w >= 0) || all(w<=0))
+	# re-sort
+	if (is.unsorted(x)) {
+		xx <- sort(x,index.return=TRUE)
+		x <- xx$x
+		w <- w[xx$ix]
+	}
+	interp_trans(x=x,y=cumsum(w),na.rm=na.rm,breaks=breaks,format=format)
+}
+
+#' @title Composition of scale transforms.
+#'
+#' @description 
+#'
+#' A binary infix operator that allows one to compose together two
+#' scale transformations. We should have that the transformation
+#' \code{atrans \%of\% btrans} first applies \code{btrans}, then
+#' applies \code{atrans} to the results. This is useful for
+#' reversing scales, for example, along with other transformations.
+#'
+#' @keywords plotting
+#' @seealso \code{\link[scales]{trans_new}}.
+#'
+#' @examples 
+#' set.seed(1234)
+#' # compose transformatins with %of%:
+#' ggplot(data.frame(x=rnorm(100),y=exp(rnorm(100,mean=-2,sd=4))),aes(x=x,y=y)) + 
+#'   geom_point() + 
+#'   scale_y_continuous(trans=scales::reverse_trans() %of% scales::log10_trans())
+#'
+#' @param atrans a transformation object.
+#' @param btrans a transformation object.
+#'
+#' @template etc
+#' @return a transformation object that perfroms \code{atrans} on the output of \code{btrans}.
+#' @usage atrans \%of\% btrans
+#' @export
+#' @rdname compose_transforms
+`%of%` <- function(atrans,btrans) {
+	awise_domain <- btrans$inverse(atrans$domain)
+	bwise_domain <- btrans$domain
+	domain <- c(max(min(awise_domain),min(bwise_domain)),
+							min(max(awise_domain),max(bwise_domain)))
+	scales::trans_new(name      = paste(atrans$name,'of',btrans$name),
+										transform = function(x) atrans$transform(btrans$transform(x)),
+										inverse   = function(y) btrans$inverse(atrans$inverse(y)),
+										breaks    = btrans$breaks,
+										format    = btrans$format,
+										domain    = domain)
+}
+
+#for vim modeline: (do not edit)
+# vim:fdm=marker:fmr=FOLDUP,UNFOLD:cms=#%s:syn=r:ft=r
